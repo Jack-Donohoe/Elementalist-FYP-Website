30a2b1cc320803206d5a23241dbcb86b3723f455
Jack Donohoe <20093738@mail.wit.ie>
2024-01-30 11:54:47 +0000

    Added basic web layout

---
+++ fyp-website/src/components/ImageCard/index.js
@@ -0,0 +1,13 @@
+import {React} from 'react';
+import { Typography, Card } from '@mui/material';
+
+const ImageCard = ({imageURL, caption, altText}) => {
+    return (
+        <Card sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black', height: '95%'}}>
+            <img src = {imageURL} alt = {altText} style={{width: '100%', height: '100%', display: 'flex'}}/>
+            {caption && <Typography sx={{color: 'white'}} variant="p">{caption}</Typography>}
+        </Card>
+    )
+}
+
+export default ImageCard;
--- fyp-website/src/pages/homePage/index.js
+++ fyp-website/src/pages/homePage/index.js
@@ -1,5 +1,50 @@
 import {React} from 'react';
+import {Container, Box, Typography, Divider, Grid} from '@mui/material';
+import data from "../../data/main.json";
+import ImageCard from '../../components/ImageCard';
 
-const homePage = () =>{
-    
+const HomePage = () =>{
+    const {
+        project_description,
+        logo_image,
+        placeholder_image
+    } = data;
+
+    const logo = require ("../../images/" + logo_image);
+    const placeholder = require ("../../images/" + placeholder_image);
+
+    return(
+        <Container sx= {{alignContent: 'center', alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column',width:'75%', padding:'2em', margin:'2em auto'}}>
+            <Box sx={{backgroundColor: 'black', padding: '1em', borderRadius: '2em', display: 'flex', width: '95%', justifyContent: 'center', alignItems: 'center'}}>
+                <Typography sx={{color: 'white'}} variant='h2'>Elementalist</Typography>
+                <img src={logo} alt="Elementalist Game Logo" width="80" height="70"  style={{ display: 'flex', marginLeft: '1em' }}/>
+            </Box>
+            <Divider sx={{width: '100%', margin: '1em 0'}} />
+
+            <Grid container spacing={2} sx={{width: '100%', margin: '1em 0'}}>
+                <Grid item xs={7} md={7}>
+                    <Box sx={{backgroundColor: 'black', padding: '1em', borderRadius: '2em', display: 'flex', flexDirection: 'column', height: '94%'}}>
+                        <Typography sx={{color: 'white'}} variant='h5'>
+                        Project Description
+                        </Typography>
+                        <Typography sx={{color: 'white', textAlign : 'justify'}} variant='p'>
+                        {project_description}
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={5} md={5}>
+                    <Box sx={{backgroundColor: 'black', padding: '1em', borderRadius: '2em', display: 'flex', flexDirection: 'column'}}>
+                        <ImageCard imageURL={placeholder} altText="Developer Image" caption="Jack Donohoe"></ImageCard>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={12}>
+                    <Box sx={{backgroundColor: 'black', padding: '1em', borderRadius: '2em', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                        <Typography sx={{color: 'white'}} variant='h5'>Demo Video</Typography>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
+
+export default HomePage;
