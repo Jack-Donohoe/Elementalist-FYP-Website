fc39cd64d3e57eb286a0880605502279520b0045
Jack Donohoe <20093738@mail.wit.ie>
2024-02-07 13:14:56 +0000

    Added Dev Image and Academic Title

--- fyp-website/src/components/ImageCard/index.js
+++ fyp-website/src/components/ImageCard/index.js
@@ -4,7 +4,7 @@ import { Typography, Card } from '@mui/material';
 const ImageCard = ({imageURL, caption, altText}) => {
     return (
         <Card sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', backgroundColor: 'black', height: '95%'}}>
-            <img src = {imageURL} alt = {altText} style={{width: '100%', height: '100%', display: 'flex'}}/>
+            <img src = {imageURL} alt = {altText} style={{width: '100%', height: '100%', display: 'flex', borderRadius: '1em'}}/>
             {caption && <Typography sx={{color: 'white'}} variant="p">{caption}</Typography>}
         </Card>
     )
--- fyp-website/src/pages/homePage/index.js
+++ fyp-website/src/pages/homePage/index.js
@@ -7,17 +7,22 @@ const HomePage = () =>{
     const {
         project_description,
         logo_image,
-        placeholder_image
+        placeholder_image,
+        developer_image
     } = data;
 
     const logo = require ("../../images/" + logo_image);
     const placeholder = require ("../../images/" + placeholder_image);
+    const dev = require ("../../images/" + developer_image);
 
     return(
-        <Container sx= {{alignContent: 'center', alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column',width:'75%', padding:'2em', margin:'2em auto'}}>
-            <Box sx={{backgroundColor: 'black', padding: '1em', borderRadius: '2em', display: 'flex', width: '95%', justifyContent: 'center', alignItems: 'center'}}>
-                <Typography sx={{color: 'white'}} variant='h2'>Elementalist</Typography>
-                <img src={logo} alt="Elementalist Game Logo" width="70" height="70"  style={{ display: 'flex', marginLeft: '1em' }}/>
+        <Container sx= {{alignContent: 'center', alignItems: 'center', justifyContent: 'center', display: 'flex', flexDirection: 'column',width:'85%', padding:'2em', margin:'2em auto'}}>
+            <Box sx={{backgroundColor: 'black', padding: '1em', borderRadius: '2em', display: 'flex', flexDirection: 'column', height: '100%', width: '95%', alignItems: 'center'}}>
+                <Box sx={{backgroundColor: 'black', padding: '1em', borderRadius: '2em', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <Typography sx={{color: 'white'}} variant='h2'>Elementalist</Typography>
+                    <img src={logo} alt="Elementalist Game Logo" width="70" height="70"  style={{ display: 'flex', marginLeft: '1em' }}/>
+                </Box>
+                <Typography sx={{color: 'white', fontStyle: 'italic'}} variant='h5'>3D Turn Based Game with Elemental Combat System </Typography>
             </Box>
             <Divider sx={{width: '100%', margin: '1em 0'}} />
 
@@ -30,11 +35,15 @@ const HomePage = () =>{
                         <Typography sx={{color: 'white', textAlign : 'justify'}} variant='p'>
                         {project_description}
                         </Typography>
+                        <Divider sx={{width: '100%', margin: '1em 0'}} />
+                        <Typography sx={{color: 'white'}} variant='h5'>
+                        Project Resources
+                        </Typography>
                     </Box>
                 </Grid>
                 <Grid item xs={5} md={5}>
                     <Box sx={{backgroundColor: 'black', padding: '1em', borderRadius: '2em', display: 'flex', flexDirection: 'column'}}>
-                        <ImageCard imageURL={placeholder} altText="Developer Image" caption="Jack Donohoe"></ImageCard>
+                        <ImageCard imageURL={dev} altText="Developer Image" caption="Jack Donohoe"></ImageCard>
                     </Box>
                 </Grid>
                 <Grid item xs={12} md={12}>
